faf52bbff32ea960ec90a63947d2d864f2296da7
Tuture57 <41503161+Tuture57@users.noreply.github.com>
2022-05-23 17:49:42 +0200

    Add files via upload

---
+++ TicTacToe/game.py
@@ -0,0 +1,67 @@
+import tkinter
+from tkinter import *
+from tkinter import messagebox
+import random as r
+
+def button(master):
+    leBouton = Button(master,bg="white",width=3,text="   ",font=('arial',60,'bold'),bd=1)
+    return leBouton
+
+def playerPlaying():
+    global a
+    if a == "X": a = "O"
+    elif a == "O": a = "X"
+
+def gameReset():
+    for i in range(3):
+        for j in range(3):
+            grid[i][j]["text"] = ""
+            grid[i][j]["state"] = NORMAL
+
+def checkWin():
+    global a
+    for i in range(3):
+        if grid[i][0]["text"] == grid[i][1]["text"] == grid[i][2]["text"] == a or grid[0][i]["text"] == grid[1][i]["text"] == grid[2][i]["text"] == a:
+            messagebox.showinfo("Victoire",""+a+" a gagné !")
+            gameReset()
+
+        elif grid[0][0]["text"] == grid[1][1]["text"] == grid[2][2]["text"] == a or grid[0][2]["text"] == grid[1][1]["text"] == grid[2][0]["text"] == a:
+            messagebox.showinfo("Victoire",""+a+" a gagné !")
+            gameReset()
+
+        elif grid[0][0]["state"] == grid[0][1]["state"] == grid[0][2]["state"] == grid[1][0]["state"] == grid[1][1]["state"] == grid[1][2]["state"] == grid[2][0]["state"] == grid[2][1]["state"] == grid[2][2]["state"] == DISABLED:
+            messagebox.showinfo("Egalité","Le jeu se finit sur une égalité !")
+            gameReset()
+
+def buttonClick(row,col):
+    global a
+    grid[row][col].config(text=a, state=DISABLED, disabledforeground=color[a])
+    checkWin()
+    playerPlaying()
+    label.config(text = "C'est au tour de " + a)
+
+#Main variables
+
+root = Tk()
+root.title("TIIIIIIIIIIIIIIIC tac toe")
+root.geometry("460x530")
+root.resizable(height=False, width=False)
+root.configure(bg="white")
+a = r.choice(["O","X"])
+color = {"O": "red", "X": "blue"}
+
+#Grid def
+
+grid = [[],[],[]]
+
+for i in range(3):
+    for j in range(3):
+        grid[i].append(button(root))
+        grid[i][j].config(command=lambda row=i,col=j:buttonClick(row,col))
+        grid[i][j].grid(row=i, column=j)
+
+#Label def
+
+label = Label(text = "C'est au tour de " + a, font=("arial",20,"bold"))
+label.grid(row=3, column=0, columnspan=3)
+root.mainloop()
--- TicTacToe/main.py
+++ TicTacToe/main.py
@@ -2,65 +2,39 @@ import tkinter
 from tkinter import *
 from tkinter import messagebox
 import random as r
+import sys
+import os
 
-def button(master):
-    leBouton = Button(master,bg="white",width=3,text="   ",font=('arial',60,'bold'),bd=1)
-    return leBouton
 
-def playerPlaying():
-    global a
-    if a == "X": a = "O"
-    elif a == "O": a = "X"
+clef = 97
 
-def gameReset():
-    for i in range(3):
-        for j in range(3):
-            grid[i][j]["text"] = ""
-            grid[i][j]["state"] = NORMAL
 
-def checkWin():
-    global a
-    for i in range(3):
-        if grid[i][0]["text"] == grid[i][1]["text"] == grid[i][2]["text"] == a or grid[0][i]["text"] == grid[1][i]["text"] == grid[2][i]["text"] == a:
-            messagebox.showinfo("Victoire",""+a+" a gagné !")
-            gameReset()
+fenetre = Tk()
+fenetre.config(bg = "#87CEEB") #Couleur fond d'écran
+fenetre.geometry("640x480")
+fenetre.resizable(height=False, width=False)
+label = Label(fenetre, text="Bienvenue sur le jeu",padx=100, pady=50)
+label.pack()
 
-        elif grid[0][0]["text"] == grid[1][1]["text"] == grid[2][2]["text"] == a or grid[0][2]["text"] == grid[1][1]["text"] == grid[2][0]["text"] == a:
-            messagebox.showinfo("Victoire",""+a+" a gagné !")
-            gameReset()
+def play():
+    os.system('py ./game.py')
 
-        elif grid[0][0]["state"] == grid[0][1]["state"] == grid[0][2]["state"] == grid[1][0]["state"] == grid[1][1]["state"] == grid[1][2]["state"] == grid[2][0]["state"] == grid[2][1]["state"] == grid[2][2]["state"] == DISABLED:
-            messagebox.showinfo("Egalité","Le jeu se finit sur une égalité !")
-            gameReset()
+def server():
+    os.system('py ./serv.py')
 
-def buttonClick(row,col):
-    global a
-    grid[row][col].config(text=a, state=DISABLED, disabledforeground=color[a])
-    checkWin()
-    playerPlaying()
-    label.config(text = "C'est au tour de " + a)
+def client():
+    os.system('py ./client.py')
 
-#Main variables
 
-root = Tk()
-root.title("TIIIIIIIIIIIIIIIC tac toe")
-root.geometry("800x1200")
-root.configure(bg="white")
-a = r.choice(["O","X"])
-color = {"O": "red", "X": "blue"}
 
-#Grid def
+play = Button(fenetre, text="Play", command = play)
+play.pack()
+server = Button(fenetre, text="Serveur", command = server)
+server.pack()
+client = Button(fenetre, text="Client", command = client)
+client.pack()
 
-grid = [[],[],[]]
 
-for i in range(3):
-    for j in range(3):
-        grid[i].append(button(root))
-        grid[i][j].config(command=lambda row=i,col=j:buttonClick(row,col))
-        grid[i][j].grid(row=i, column=j)
+fenetre.mainloop()
 
-#Label def
 
-label = Label(text = "C'est au tour de " + a, font=("arial",20,"bold"))
-label.grid(row=3, column=0, columnspan=3)
-root.mainloop()
